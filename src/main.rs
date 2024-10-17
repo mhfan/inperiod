8d359fe5d3c2325c9f97df6d2824a7ee663832d4
MeiHui FAN <mhfan@ustc.edu>
2024-10-17 15:13:25 +0800

    refined sample code

--- src/main.rs
+++ src/main.rs
@@ -3,55 +3,62 @@
 use dioxus::prelude::*;
 use dioxus_logger::tracing;
 
-
-#[derive(Clone, Routable, Debug, PartialEq, serde::Serialize, serde::Deserialize)]
-enum Route {
-    #[route("/")]
-    Home {},
-    #[route("/blog/:id")]
-    Blog { id: i32 },
-}
-
+// Urls are relative to your Cargo.toml file
+//const _TAILWIND_URL: &str = manganis::mg!(file("assets/tailwind.css"));
 
 fn main() {
-    // Init logger
     dioxus_logger::init(tracing::Level::INFO).expect("failed to init logger");
-    tracing::info!("starting app");
-    launch(App);
+    //tracing::info!("starting app");
+
+    #[cfg(not(feature = "desktop"))] launch(App);
+    #[cfg(feature = "desktop")] LaunchBuilder::desktop().with_cfg(dioxus::desktop::Config::new()
+        .with_window(dioxus::desktop::WindowBuilder::new().with_title(env!("CARGO_PKG_NAME")))
+        .with_custom_head("<link rel='stylesheet' href='tailwind.css'/>".into())
+        //.with_custom_head("<script  src='https://cdn.tailwindcss.com'/>".into())
+        //.with_custom_index(r"<!DOCTYPE html><html>...</html>".into())
+    ).launch(App);
 }
 
-
 fn App() -> Element {
-    rsx! {
-        Router::<Route> {}
+    rsx! {  //Router::<Route> {}
+        link { rel: "stylesheet", href: "main.css" }
+        img { src: "header.svg", id: "header" }
+        div { id: "links",
+            a { target: "_blank", href: "https://dioxuslabs.com/learn/0.5/", "📚 Learn Dioxus" }
+            a { target: "_blank", href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
+            a { target: "_blank", href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
+            a { target: "_blank", href: "https://github.com/DioxusLabs/dioxus-std", "⚙️ Dioxus Standard Library" }
+            a { target: "_blank", href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
+            a { target: "_blank", href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
+        }
     }
 }
 
-#[component]
-fn Blog(id: i32) -> Element {
+/* for fullstack web renderer
+#[derive(Clone, Routable, Debug, PartialEq, serde::Serialize, serde::Deserialize)]
+enum Route {
+    #[route("/")] Home {},
+    #[route("/blog/:id")] Blog { id: i32 },
+}
+
+#[component] fn Blog(id: i32) -> Element {
     rsx! {
         Link { to: Route::Home {}, "Go to counter" }
         "Blog post {id}"
     }
 }
 
-#[component]
-fn Home() -> Element {
+#[component] fn Home() -> Element {
     let mut count = use_signal(|| 0);
     let mut text = use_signal(|| String::from("..."));
 
     rsx! {
-        Link {
-            to: Route::Blog {
-                id: count()
-            },
-            "Go to blog"
-        }
+        Link { to: Route::Blog { id: count() }, "Go to blog" }
         div {
             h1 { "High-Five counter: {count}" }
             button { onclick: move |_| count += 1, "Up high!" }
             button { onclick: move |_| count -= 1, "Down low!" }
-            button {
+            #[cfg(feature = "server")] button {
                 onclick: move |_| async move {
                     if let Ok(data) = get_server_data().await {
                         tracing::info!("Client received: {}", data);
@@ -66,14 +73,14 @@ fn Home() -> Element {
     }
 }
 
-
-#[server(PostServerData)]
+#[cfg(feature = "server")] #[server(PostServerData)]
 async fn post_server_data(data: String) -> Result<(), ServerFnError> {
     tracing::info!("Server received: {}", data);
     Ok(())
 }
 
-#[server(GetServerData)]
+#[cfg(feature = "server")] #[server(GetServerData)]
 async fn get_server_data() -> Result<String, ServerFnError> {
     Ok("Hello from the server!".to_string())
-}
+} */
+
